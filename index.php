c45148cf8539e717e0ef05f2e141599d373159c9
Al Rogatto <alrogattodev@gmail.com>
2021-03-09 01:03:00 -0300

    two columns grid layout

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="//db.onlinewebfonts.com/c/750d94312f5c64bdf8b1fc783a4cda89?family=BentonSans-Book" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<div class="container">
+	  <main></main>
+	  <aside>
+		<p class="title">Digite o CPF do Titular</p>
+		<p class="text">para localizarmos seu cadastro e iniciarmos o procedimento para a alteração do cartão de crédito do seu contrato</p>
+		<input type="text" placeholder="CPF"/>
+		<button>Enviar</button>
+	  </aside>
+	</div>
+</body>
+</html>
